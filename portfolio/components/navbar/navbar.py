ab2dc2f838f0cb291bce306ea4a38a138ea79cae
LuMi <lumigu.dev@gmail.com>
2023-11-13 20:42:19 -0500

    Added links and navigation

--- portfolio/components/navbar/navbar.py
+++ portfolio/components/navbar/navbar.py
@@ -1,19 +1,56 @@
 import reflex as rx
 
+from portfolio.style.styles import Size
+import portfolio.style.styles as styles
+
 
 def navbar() -> rx.Component:
     return rx.hstack(
-        rx.hstack(
-            rx.image(src="favicon.ico"),
-            rx.heading("My App"),
+        rx.link(
+            rx.hstack(
+                rx.image(src="favicon.ico"),
+                rx.heading(
+                    "LuPy",
+                    style=styles.navbar_title_style,
+                ),
+            ),
+            href="",
         ),
-        rx.spacer(),
         rx.hstack(
-            rx.image(src="favicon.ico"),
-            rx.heading("My App"),
+            rx.link(
+                "Portafolio",
+                margin_left=Size.ZERO.value,
+                padding_x=Size.SMALL.value,
+                border_right="1px solid #000",
+                href="#portafolio",
+            ),
+            rx.link(
+                "Experiencia",
+                margin_left=Size.ZERO.value,
+                padding_x=Size.SMALL.value,
+                border_right="1px solid #000",
+                href="#experiencia",
+            ),
+            rx.link(
+                "Contacto",
+                margin_left=Size.ZERO.value,
+                padding_x=Size.SMALL.value,
+                border_right="1px solid #000",
+                href="#contacto",
+            ),
+            rx.link(
+                rx.image(
+                    src="assets/icons/dark_mode.svg",
+                ),
+                margin_left=Size.ZERO.value,
+                padding_x=Size.SMALL.value,
+            ),
         ),
         position="sticky",
-        width="100%",
-        top="0px",
+        justify="space-between",
+        padding_x=Size.EXTREME_BIG.value,
+        padding_y=Size.DEFAULT.value,
+        top="0",
         z_index="999",
+        id="home",
     )
