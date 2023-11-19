44aacebcd207d1349c2118b64c8ff548c6b16dcf
LuMi <lumigu.dev@gmail.com>
2023-11-18 20:02:43 -0500

    Added colors

---
+++ portfolio/style/colors.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class Color(Enum):
+    PRIMARY = "#7833fe"
+    PRIMARY_LIGHT = "#974cff"
+    SECUNDARY = "#05bdfb"
+    SECUNDARY_LIGHT = "#60e6ff"
+    BACKGROUND = "#05192d"
+    BACKGROUND_CONTAINER = "#213147"
+
+
+class TextColor(Enum):
+    TITLE = "#FFFFF2"
+    BODY = "#9ba3ab"
