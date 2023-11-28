ec70bef1fb503aa6610ac69ea656538589a8c0e7
LuMi <lumigu.dev@gmail.com>
2023-11-28 10:44:59 -0500

    Responsive created

--- portfolio/views/experience/experience.py
+++ portfolio/views/experience/experience.py
@@ -10,52 +10,138 @@ from portfolio.components.experience.right import experience_right
 
 def experience() -> rx.Component:
     return rx.vstack(
-        rx.heading(
-            "Experiencia",
-            justify_item="start",
-            margin_bottom=styles.Size.SMALL.value,
-            style=styles.header_title_style,
-            padding_top=styles.Size.VERY_BIG.value,
+        rx.desktop_only(
+            rx.heading(
+                "Experiencia",
+                text_align="center",
+                margin_bottom=styles.Size.SMALL.value,
+                style=styles.header_title_style,
+                padding_top=styles.Size.VERY_BIG.value,
+            ),
+            rx.grid(
+                rx.grid_item(
+                    experience_left(
+                        "Backend Developer Trainee",
+                        "NoCountry",
+                        f"Oct 2023 - {date.today().strftime('%b')} {date.today().year}",
+                        list_icon_left(
+                            "arrow_left",
+                            Color.SECUNDARY.value,
+                            "Participé en la creación de un MVP desde cero",
+                        ),
+                        list_icon_left(
+                            "arrow_left",
+                            Color.SECUNDARY.value,
+                            "Apoyé en la creación del modelo entidad-relación de la base de datos.",
+                        ),
+                        list_icon_left(
+                            "arrow_left",
+                            Color.SECUNDARY.value,
+                            "Desarrollé e implementé una de las RESTful APIs del MVP.",
+                        ),
+                    ),
+                    height="fit-content",
+                    border_bottom_right_radius=styles.Size.DEFAULT.value,
+                ),
+                rx.grid_item(
+                    rx.box(
+                        width="100%",
+                        height="100%",
+                        bg=Color.BACKGROUND.value,
+                    ),
+                ),
+                # rx.grid_item(
+                #     rx.box(
+                #         width="100%",
+                #         height="100%",
+                #         bg=Color.BACKGROUND.value,
+                #     ),
+                # ),
+                # rx.grid_item(
+                #     experience_right(
+                #         "Backend Developer Trainee",
+                #         "NoCountry",
+                #         f"Oct 2023 - {date.today().strftime('%b')} {date.today().year}",
+                #         list_icon_right(
+                #             "arrow_right",
+                #             Color.SECUNDARY.value,
+                #             "Participé en la creación de un MVP desde cero.",
+                #         ),
+                #         list_icon_right(
+                #             "arrow_right",
+                #             Color.SECUNDARY.value,
+                #             "Apoyé en la creación del modelo entidad-relación de la base de datos.",
+                #         ),
+                #         list_icon_right(
+                #             "arrow_right",
+                #             Color.SECUNDARY.value,
+                #             "Desarrollé e implementé una de las RESTful APIs del MVP.",
+                #         ),
+                #     ),
+                #     border_bottom_left_radius=styles.Size.DEFAULT.value,
+                # ),
+                template_columns="repeat(2, 1fr)",
+                width="100%",
+                height="auto",
+            ),
         ),
-        rx.grid(
-            rx.grid_item(
-                experience_left(
-                    "Backend Developer Trainee",
-                    "NoCountry",
-                    f"Oct 2023 - {date.today().strftime('%b')} {date.today().year}",
-                    list_icon_left(
-                        "arrow_left",
-                        Color.SECUNDARY.value,
-                        "Participé en la creación de un MVP desde cero",
-                    ),
-                    list_icon_left(
-                        "arrow_left",
-                        Color.SECUNDARY.value,
-                        "Apoyé en la creación del modelo entidad-relación de la base de datos.",
-                    ),
-                    list_icon_left(
-                        "arrow_left",
-                        Color.SECUNDARY.value,
-                        "Desarrollé e implementé una de las RESTful APIs del MVP.",
-                    ),
+        rx.tablet_only(
+            rx.box(
+                rx.heading(
+                    "Experiencia",
+                    text_align="center",
+                    margin_bottom=styles.Size.SMALL.value,
+                    style=styles.header_title_style,
+                    padding_top=styles.Size.VERY_BIG.value,
                 ),
-                border_bottom_right_radius=styles.Size.DEFAULT.value,
-            ),
-            rx.grid_item(
-                rx.box(
+                rx.grid(
+                    rx.grid_item(
+                        experience_left(
+                            "Backend Developer Trainee",
+                            "NoCountry",
+                            f"Oct 2023 - {date.today().strftime('%b')} {date.today().year}",
+                            list_icon_left(
+                                "arrow_left",
+                                Color.SECUNDARY.value,
+                                "Participé en la creación de un MVP desde cero",
+                            ),
+                            list_icon_left(
+                                "arrow_left",
+                                Color.SECUNDARY.value,
+                                "Apoyé en la creación del modelo entidad-relación de la base de datos.",
+                            ),
+                            list_icon_left(
+                                "arrow_left",
+                                Color.SECUNDARY.value,
+                                "Desarrollé e implementé una de las RESTful APIs del MVP.",
+                            ),
+                        ),
+                        height="fit-content",
+                        border_bottom_right_radius=styles.Size.DEFAULT.value,
+                    ),
+                    rx.grid_item(
+                        rx.box(
+                            width="100%",
+                            height="100%",
+                            bg=Color.BACKGROUND.value,
+                        ),
+                    ),
+                    template_columns="repeat(2, 1fr)",
                     width="100%",
-                    height="100%",
-                    bg=Color.BACKGROUND.value,
+                    height="auto",
                 ),
+                margin_x=styles.Size.VERY_BIG.value,
             ),
-            rx.grid_item(
-                rx.box(
-                    width="100%",
-                    height="100%",
-                    bg=Color.BACKGROUND.value,
+        ),
+        rx.mobile_only(
+            rx.box(
+                rx.heading(
+                    "Experiencia",
+                    justify_item="start",
+                    margin_bottom=styles.Size.SMALL.value,
+                    style=styles.header_title_style,
+                    padding_top=styles.Size.BIG.value,
                 ),
-            ),
-            rx.grid_item(
                 experience_right(
                     "Backend Developer Trainee",
                     "NoCountry",
@@ -75,12 +161,10 @@ def experience() -> rx.Component:
                         Color.SECUNDARY.value,
                         "Desarrollé e implementé una de las RESTful APIs del MVP.",
                     ),
+                    bg=Color.BACKGROUND_CONTAINER.value,
                 ),
-                border_bottom_left_radius=styles.Size.DEFAULT.value,
+                margin_x=styles.Size.DEFAULT.value,
             ),
-            template_columns="repeat(2, 1fr)",
-            width="100%",
-            height="100%",
         ),
         id="experiencia",
     )
