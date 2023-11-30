4c54f1cf9e975a5e6995e7af3788f57758e75904
LuMi <lumigu.dev@gmail.com>
2023-11-30 01:20:03 -0500

    Added metadata

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -1,14 +1,30 @@
 import reflex as rx
-
-
-class State(rx.State):
-    pass
+import portfolio.style.styles as styles
+from portfolio.components.navbar.navbar import navbar
+from portfolio.components.footer.footer import footer
+from portfolio.views.header.header import header
 
 
 def index() -> rx.Component:
-    return rx.text("Hola")
+    return rx.box(
+        navbar(),
+        rx.center(
+            rx.vstack(
+                # header(),
+            ),
+        ),
+        footer(),
+        width="100%",
+    )
 
 
-app = rx.App()
-app.add_page(index)
+app = rx.App(
+    style=styles.BASE_STYLE,
+)
+app.add_page(
+    index,
+    title="Lupy | Backend Developer",
+    description="Hola, mi nombre es: Luis aka Lupy. Soy desarrollador de software especializado en el backend",
+    image="favicon.ico",
+)
 app.compile()
